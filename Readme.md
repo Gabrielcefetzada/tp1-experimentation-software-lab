95d3e0ea81a0261c01e092e4b9cf37b621852dc7
Gabrielcefetzada <gabrielaugustosb13@gmail.com>
2026-02-23 20:17:22 -0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# Setup de configuração
+
+- Tenha o Node instalado na sua máquina: https://nodejs.org/en/download
+
+- Da primeira vez, você precisará criar um token para consumir a API do Github. Faça isso no seguinte endereço: https://github.com/settings/tokens
+
+- Cole seu token no arquivo .env para na variável GITHUB_TOKEN
+
+# Rodar o script
+
+Execute: 
+
+```shell
+node --env-file=.env script.js
+```
---
+++ script.js
@@ -0,0 +1,49 @@
+async function main() {
+
+  const query = getGraphqlQuery();
+
+  const response = await fetch('https://api.github.com/graphql', {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${process.env.GITHUB_TOKEN}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: query
+    })
+  });
+
+  const data = await response.json();
+  
+  if (data.errors) {
+    throw new Error(data.errors[0].message);
+  }
+
+  console.log(JSON.stringify(data));
+  return data;
+}
+
+function getGraphqlQuery() {
+    return `
+    query {
+      search(
+        query: "stars:>50000"
+        type: REPOSITORY
+        first: 100
+      ) {
+        nodes {
+          ... on Repository {
+            name
+            owner {
+              login
+            }
+            description
+            createdAt
+          }
+        }
+      }
+    }
+  `;
+}
+
+main();
