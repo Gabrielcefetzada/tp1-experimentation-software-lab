24c87accdec44a639a0d9491bc10c504b89bd29c
Gabrielcefetzada <gabrielaugustosb13@gmail.com>
2026-02-26 22:15:05 -0300

    feat: add cursor pagination and pullrequests count

--- script.js
+++ script.js
@@ -1,35 +1,41 @@
 async function main() {
-
-  const query = getGraphqlQuery();
-
-  const response = await fetch('https://api.github.com/graphql', {
-    method: 'POST',
-    headers: {
-      'Authorization': `Bearer ${process.env.GITHUB_TOKEN}`,
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      query: query
-    })
-  });
-
-  const data = await response.json();
+  let hasNextPage = true;
+  let cursor = null;
+  let allRepos = [];
+  const pageSize = 10;
+    
+  while (hasNextPage && allRepos.length < 100) {
+    const query = getGraphqlQueryWithCursor(pageSize, cursor);
   
-  if (data.errors) {
-    throw new Error(data.errors[0].message);
+    const response = await fetchGithubApi(query);
+
+    const data = await response.json();
+    
+    if (data.errors) {
+      console.error('Erro: ', data.errors);
+      break;
+    }
+
+    const pageData = data.data.search;
+    allRepos = [...allRepos, ...pageData.nodes];
+    hasNextPage = pageData.pageInfo.hasNextPage;
+    cursor = pageData.pageInfo.endCursor;    
   }
 
-  console.log(JSON.stringify(data));
-  return data;
+  console.log(JSON.stringify({ data: { search: { nodes: allRepos } } }, null, 2));
+  return { data: { search: { nodes: allRepos } } };
 }
 
-function getGraphqlQuery() {
+function getGraphqlQueryWithCursor(first = 20, cursor = null) {
+  const afterParam = cursor ? `after: "${cursor}"` : '';
+  
   return `
     query {
       search(
         query: "stars:>50000"
         type: REPOSITORY
-        first: 100
+        first: ${first}
+        ${afterParam}
       ) {
         nodes {
           ... on Repository {
@@ -39,17 +45,35 @@ function getGraphqlQuery() {
             }
             description
             createdAt
-
             updatedAt  
-
-            releases {  
+            releases {
+              totalCount
+            }
+            pullRequests {
               totalCount
             }
           }
         }
+        pageInfo {
+          hasNextPage
+          endCursor
+        }
       }
     }
   `;
+}
+
+async function fetchGithubApi(query)
+{
+  return response = await fetch('https://api.github.com/graphql', {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${process.env.GITHUB_TOKEN}`,
+        'Content-Type': 'application/json',
+        'User-Agent': 'Node.js-App'
+      },
+      body: JSON.stringify({ query })
+    });
 }
 
 main();
